599bb0fedcb40e750fa5b16cc4faa2664b4ba5e6
Mateusz Walczyk <mattiusw@gmail.com>
2024-10-12 12:01:08 +0200

    Checking data proportional

--- first_project_ml/load_housint_data.py
+++ first_project_ml/load_housint_data.py
@@ -43,13 +43,29 @@ def main():
     housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
     train_set, test_set = split_data_with_id_hash(housing_with_id, 0.2, "id")
     train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+    # Set income mediana
     housing["income_cat"] = pd.cut(housing["median_income"],
             bins=[0.,1.5,3.0,4.5,6.,np.inf],
             labels=[1,2,3,4,5])
     housing["income_cat"].value_counts().sort_index().plot.bar(rot=0, grid=True)
     plt.xlabel("Kategoria dochodów")
     plt.ylabel("Liczba dystryktów")
-    housing.hist(bins=50, figsize=(12,8)) # Set graphs 
+    housing.hist(bins=50, figsize=(12,8)) # Set all data graphs 
+    
+    # Stratifield sampling
+    splitter = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
+    strat_splits = []
+    for train_index, test_index in splitter.split(housing, housing["income_cat"]):
+        strat_train_set_n = housing.iloc[train_index]
+        strat_test_set_n = housing.iloc[test_index]
+        strat_splits.append([strat_train_set_n, strat_test_set_n])
+    
+    # Check proportional data on example
+    strat_train_set, strat_test_set = strat_splits[0]
+    strat_train_set, strat_test_set = train_test_split(housing, test_size=0.2, stratify=housing["income_cat"], random_state=42)
+    
+    print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+
     plt.show() # View graph
 
 if __name__ == "__main__":
