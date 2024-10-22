565b2f7af56fcceb901954ac90346732724502f3
Mateusz Walczyk <mattiusw@gmail.com>
2024-10-22 20:04:33 +0200

    Simply shuffle data with function sklearn

--- first_project_ml/Main.py
+++ first_project_ml/Main.py
@@ -1,11 +1,19 @@
+from pandas.core.common import random_state
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 import load_housint_data
 from PrepareData import PrepareDate
+from PrepareDataV2 import PrepareDataV2
 
 
 def main():
     housing = load_housint_data.load_housing_data()
-    housing_with_id = PrepareDate(housing)
-    print(housing_with_id)
+    housing_with_sklearn = PrepareDataV2()
+    housing_with_sklearn.preparing_data(housing)
+    strat_test_set = housing_with_sklearn.get_strat_test_set()
+
+    print("Próbkowanie za pomocą funkcji wbudowanej w bibliotekę: ")
+    print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
 
 if __name__ == "__main__":
     main()
---
+++ first_project_ml/PrepareDataV2.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+class PrepareDataV2():
+    def __init__(self):
+        self.strat_train_set = []
+        self.strat_test_set = []
+
+    def preparing_data(self, data_frame):
+        data_frame["income_cat"] = pd.cut(data_frame["median_income"],bins=[0.,1.5,3.0,4.5,6.,np.inf], labels=[1,2,3,4,5])
+        # Próbkowanie warstwowe za pomocą funkcji sklearn
+        strat_train_set, strat_test_set = train_test_split(data_frame, test_size=0.2, stratify=data_frame["income_cat"],
+                                                           random_state=42)
+        self.strat_train_set = strat_train_set
+        self.strat_test_set = strat_test_set
+
+    def get_strat_train_set(self):
+        return self.strat_train_set
+
+    def get_strat_test_set(self):
+        return self.strat_test_set
