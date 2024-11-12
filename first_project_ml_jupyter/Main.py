f771222d18dfe49e7d522dfb687d849b93b975dd
Mateusz Walczyk <mattiusw@gmail.com>
2024-11-12 18:50:22 +0100

    Wyświetlenie histogram dochodów

--- first_project_ml_jupyter/Main.py
+++ first_project_ml_jupyter/Main.py
@@ -4,8 +4,6 @@ from LoadData import load_test_set
 def main():
     train_set, test_set = load_test_set()
 
-    print(len(train_set))
-    print(len(test_set))
 
 if __name__ == "__main__":
     main()
