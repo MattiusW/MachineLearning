91bc1ec929a4f893ff6adbdafcc48f73007891f1
Mateusz Walczyk <mattiusw@gmail.com>
2024-11-19 10:32:58 +0100

    Zastapienie brakujacych danych mediania

--- first_project_ml_jupyter/Main.py
+++ first_project_ml_jupyter/Main.py
@@ -1,7 +1,10 @@
 from LoadData import *
-
+from PrepareData import PrepareDate
 
 def main():
+    prepare_data = PrepareDate()
+
+    housing = load_housing_data()
     train_set, test_set = load_test_set()
     strat_train_set, strat_test_set = load_spliter_test_set()
     test = strat_test_set["income_cat"].value_counts() / len(strat_test_set)
@@ -9,6 +12,7 @@ def main():
 
     # Usunac kolumne inf
     delete_inf_column(strat_train_set, strat_test_set)
+    
 
 if __name__ == "__main__":
     main()
--- first_project_ml_jupyter/PrepareData.py
+++ first_project_ml_jupyter/PrepareData.py
@@ -1,9 +1,16 @@
 import numpy as np
+from sklearn.impute import SimpleImputer
 
 class PrepareDate:
     def __init__(self):
         pass
 
-    def column_string_to_int(data):
+    def column_string_to_int(self, data):
         housing_num = data.select_dtypes(include=[np.number])
         return housing_num
+    
+    def impute_data(self, data):
+        imputer = SimpleImputer(strategy="median")
+        imputer.fit(data)
+        X = imputer.transform(data)
+        return X
